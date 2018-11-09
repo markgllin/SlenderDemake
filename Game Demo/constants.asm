5230731e962122c6ff84a00fe575af797f0d0fe3
markgllin <markgllin@hotmail.com>
2018-11-08 20:58:16 -0700

    ocd

--- Game Demo/constants.asm
+++ Game Demo/constants.asm
@@ -1,84 +1,84 @@
 ;;; ---- ADDRESSES
-AUX_C     = $900e	; bits 4-7: auxillary colour
-SCR_C     = $900f	; bits 0-2: border color 
-                        ; bits 4-7: background color
-CHR_C     = $0286	; character color (dec: 646)
+AUX_C             = $900e   ; bits 4-7: auxillary colour
+SCR_C             = $900f   ; bits 0-2: border color 
+                                  ; bits 4-7: background color
+CHR_C             = $0286   ; character color (dec: 646)
 
-COLOR_MEM = $9600	; start of colour memory 
-SCRN_MEM1 = $1e00	; start of screen memory
-SCRN_MEM2 = $1f00	; middle of screen memory
-CHAR_MEM  = $1c00	; start of character memory
+COLOR_MEM         = $9600   ; start of colour memory 
+SCRN_MEM1         = $1e00   ; start of screen memory
+SCRN_MEM2         = $1f00   ; middle of screen memory
+CHAR_MEM          = $1c00   ; start of character memory
 
-SCRN_V    = $9001	; vertical origin of the screen
-ROWS	  = $9003	; bits 1-6: number of rows (ref: p 175)
-RASTER	  = $9004
+SCRN_V            = $9001   ; vertical origin of the screen
+ROWS	            = $9003   ; bits 1-6: number of rows (ref: p 175)
+RASTER	          = $9004
 
 ;; The IRQ vector is the location (ISR) to jump when an interrupt happens
-IRQ_L        = $0314		; low byte of IRQ vector 
-IRQ_H        = $0315		; high byte of IRQ vector 
-OLD_IRQ      = $eabf
+IRQ_L             = $0314		; low byte of IRQ vector 
+IRQ_H             = $0315		; high byte of IRQ vector 
+OLD_IRQ           = $eabf
 	
-VIA_CONTROL  = $911b
-VIA_FLAGS    = $911d		; reference: page 218
-VIA_ENABLE   = $911e
+VIA_CONTROL       = $911b
+VIA_FLAGS         = $911d		; reference: page 218
+VIA_ENABLE        = $911e
 
-TIMER1_LOC   = $9114		; timer 1 low order counter
-TIMER1_HOC   = $9115		; timer 1 high order counter
-TIMER1_LOL   = $9116		; timer 1 low order latch
-TIMER1_HOL   = $9117		; timer 1 high order latch
+TIMER1_LOC        = $9114		; timer 1 low order counter
+TIMER1_HOC        = $9115		; timer 1 high order counter
+TIMER1_LOL        = $9116		; timer 1 low order latch
+TIMER1_HOL        = $9117		; timer 1 high order latch
 	
-TIMER2_L     = $9118		; timer 2 low order byte
-TIMER2_H     = $9119		; timer 2 high order byte
+TIMER2_L          = $9118		; timer 2 low order byte
+TIMER2_H          = $9119		; timer 2 high order byte
 
 ;;; CUSTOM CHARSET ADDRESSES
-SPACE_ADDRESS   = $1c00   ; where the space character starts
-SPRITE_ADDRESS  = $1c10		; where the sprites start
-NUMBERS_ADDRESS = $1cc0		; where numbers start in custom charset
+SPACE_ADDRESS     = $1c00   ; where the space character starts
+SPRITE_ADDRESS    = $1c10		; where the sprites start
+NUMBERS_ADDRESS   = $1cc0		; where numbers start in custom charset
 	
 
 ;;; ---- CONSTANTS
-SPACE             = #$00	; @
-CHAR_FORWARD      = #$02	; A = character facing forward
-CHAR_BCKWARD      = #$06	; E = character facing backward
-CHAR_RIGHT        = #$0a	; I = character facing to the right
-CHAR_LEFT         = #$0e	; M = character facing to the left
-TREE1             = #$12	; Q = tree sprite  
-ITEM_LETTER	  = #$16 	; U = letter sprite
-NUM_ZERO	  = #$18	; W = start of number 0
-NUM_NINE	  = #$21	; SPACE = start of number 9 
+SPACE             = #$00    ; @
+CHAR_FORWARD      = #$02    ; A = character facing forward
+CHAR_BCKWARD      = #$06    ; E = character facing backward
+CHAR_RIGHT        = #$0a    ; I = character facing to the right
+CHAR_LEFT         = #$0e    ; M = character facing to the left
+TREE1             = #$12    ; Q = tree sprite  
+ITEM_LETTER	      = #$16    ; U = letter sprite
+NUM_ZERO	        = #$18    ; W = start of number 0
+NUM_NINE	        = #$21    ; SPACE = start of number 9 
 
-SPRITE_CHAR_COLOR = #$09 	; multi-colour white
-TREE_CHAR_COLOR   = #$0d	; multi-colour green
+SPRITE_CHAR_COLOR = #$09   	; multi-colour white
+TREE_CHAR_COLOR   = #$0d   	; multi-colour green
 
 ;; The timer runs at 1MHz so that means 1,000,000 cycles per second
 ;; Hence, we need to wait 1,000,000 = $000F 4240 cycles to wait a single second
 ;; That is, wait $f424 a total of $10 = 16 times.
-SECOND_H	  = #$f4	; high byte of $f906
-SECOND_L	  = #$24	; low  byte of $f906
-NUM_SEC		  = #$11	; 1 MORE THAN how many ^ it takes for a single second
+SECOND_H	        = #$f4    ; high byte of $f906
+SECOND_L	        = #$24    ; low  byte of $f906
+NUM_SEC		        = #$11    ; 1 MORE THAN how many ^ it takes for a single second
 
-CLEAR_CHAR          = 0
+CLEAR_CHAR        = 0
 
-LFT_SCRN_BNDRY      = 0
-TOP_SCRN_BNDRY      = 0
-RGHT_SCRN_BNDRY     = 21
-BTM_SCRN_BNDRY      = 21
+LFT_SCRN_BNDRY    = 0
+TOP_SCRN_BNDRY    = 0
+RGHT_SCRN_BNDRY   = 21
+BTM_SCRN_BNDRY    = 21
 
-X_OFFSET            = 1
-Y_OFFSET            = 22
+X_OFFSET          = 1
+Y_OFFSET          = 22
 
-MAZE_ORIGIN         = 94
+MAZE_ORIGIN       = 94
 
-NORTH               = 1 
-SOUTH               = 2 
-EAST                = 3 
-WEST                = 4 
-PATH                = 1
+NORTH             = 1 
+SOUTH             = 2 
+EAST              = 3 
+WEST              = 4 
+PATH              = 1
 
-SCAN_KEYBOARD         = $c5
-W_KEY                 = 9
-A_KEY                 = 17
-S_KEY                 = 41
-D_KEY                 = 18
+SCAN_KEYBOARD     = $c5
+W_KEY             = 9
+A_KEY             = 17
+S_KEY             = 41
+D_KEY             = 18
 
-SEED                  = 240   ; can be anything but 0
+SEED              = 240     ; can be anything but 0
--- Game Demo/zero_page.asm
+++ Game Demo/zero_page.asm
@@ -1,24 +1,24 @@
 ;;; ---- ZERO PAGE
-SCRN_LSB    = $00	; LSB of screen memory address
-SCRN_MSB    = $01	; MSB Of screen memory address	
-CLRM_LSB    = $04	; LSB of colour memory address
-CLRM_MSB    = $05	; MSB Of colour memory address
+SCRN_LSB    				= $00	 ; LSB of screen memory address
+SCRN_MSB    				= $01	 ; MSB Of screen memory address	
+CLRM_LSB    				= $04	 ; LSB of colour memory address
+CLRM_MSB    				= $05	 ; MSB Of colour memory address
+ 
+GAME_STATUS 				= $07	 ; if this is 0, end game
+ 
+CURR_SPRITE 				= $08	 ; stores the starting character for current sprite
+FRAME	   	  				= $09	 ; frame counter for delays
+CURR_CLR    				= $0a	 ; stores the current character colour
+TIMER_CTR   				= $0b	 ; timer counter
+NUM_WRAPS   				= $0c	 ; used to figure out end game condition
+	               					 ; basically, counts the number of times the numbers wrap per decrement 
+	               					 ; If this reaches 3, then FULL WRAP AROUND so end game 
+SPRITE_X 						= $0d
+SPRITE_Y 						= $0e
+SPRITE_PREV_X 			= $0f
+SPRITE_PREV_Y 			= $10	
+	
 
-GAME_STATUS = $07	; if this is 0, end game
-	
-CURR_SPRITE = $08	; stores the starting character for current sprite
-FRAME	    = $09	; frame counter for delays
-CURR_CLR    = $0a	; stores the current character colour
-TIMER_CTR   = $0b	; timer counter
-NUM_WRAPS   = $0c	; used to figure out end game condition
-	                ; basically, counts the number of times the numbers wrap per decrement 
-	                ; If this reaches 3, then FULL WRAP AROUND so end game 
-SPRITE_X = $0d
-SPRITE_Y = $0e
-SPRITE_PREV_X = $0f
-SPRITE_PREV_Y = $10	
-	
-	
 ;; save the previous position of sprite incase invalid movement 
 PREV_SPRITE_LSB     = $11	
 PREV_SPRITE_MSB     = $12
@@ -26,20 +26,20 @@ PREV_SPRITE_CLR_LSB = $15
 PREV_SPRITE_CLR_MSB = $16
 
 ;; current / proposed movement of sprite 
-SPRITE_LSB	 = $18
-SPRITE_MSB       = $19
-SPRITE_CLR_LSB   = $1a
-SPRITE_CLR_MSB   = $1b
+SPRITE_LSB	 				= $18
+SPRITE_MSB      		= $19
+SPRITE_CLR_LSB  		= $1a
+SPRITE_CLR_MSB  		= $1b
 
-LETTER_LSB 	 = $1e
-LETTER_MSB	 = $1f
-LETTER_CLR_LSB   = $20
-LETTER_CLR_MSB	 = $21	
+LETTER_LSB 	 				= $1e
+LETTER_MSB	 				= $1f
+LETTER_CLR_LSB  		= $20
+LETTER_CLR_MSB			= $21	
 
-SCORE_LSB	 = $22
-SCORE_MSB	 = $23
+SCORE_LSB			  		= $22
+SCORE_MSB	      		= $23
 
-LFSR 		 = $24	
+LFSR 								= $24	
 
 OFFSET              = $25
 LSB                 = $26
@@ -51,7 +51,7 @@ MAZE_LSB            = $2a
 MAZE_MSB            = $2b
 MAZE_DIR            = $2c
 
-TREE_CLR          = $2e
-TREE_SPRITE       = $2f
-TREE_CLR_LSB      = $30
-TREE_LSB          = $31
+TREE_CLR          	= $2e
+TREE_SPRITE       	= $2f
+TREE_CLR_LSB      	= $30
+TREE_LSB          	= $31
