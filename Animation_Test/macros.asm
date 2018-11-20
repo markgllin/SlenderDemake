20c0efbdd3cf6f4fee678e2349011e9ee8a0caa7
heenawter <hannah.k.mudge@gmail.com>
2018-11-20 11:03:35 -0700

    made a code formatter

---
+++ Animation_Test/macros.asm
@@ -0,0 +1,126 @@
+; MACROS
+
+; loads coordinate into [{1}] and adds [{2}]
+; result stored in accumulator
+        mac     inc_maze_coord
+            lda     [{1}]
+            clc
+            adc     [{2}]
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; loads coordinate into [{1}] and subtracts [{2}]
+; result stored in accumulator
+        mac     dec_maze_coord
+            lda     [{1}]
+            sec
+            sbc     [{2}]
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; loads offset increment into accumulator
+; updates addresses OFFSET, MAZE_LSB, MAZE_MSB
+; executes add/subOffset in [{2}]
+; result stored in accumulator
+        mac     load_maze_offsets
+            lda     [{1}]
+            sta     OFFSET
+            lda     MAZE_LSB
+            ldx     MAZE_MSB
+            jsr     [{2}]
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; stores maze offsets into corresponding
+; addresses after offsets are updated
+; follows 'load_maze_offsets' macro
+        mac     store_maze_offsets
+            sta     MAZE_MSB
+            lda     LSB
+            sta     MAZE_LSB
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; needs sprite clr in [{1}],
+; sprite_clr_lsb in [{2}],
+; and sprite_lsb in [{3}]
+        mac     draw_sprite
+            lda     [{1}]
+            sta     ([{2}]),y
+            txa
+            sta     ([{3}]),y
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; needs sprite to draw in [{1}],
+; sprite_clr in [{2}],
+; sprite_clr_lsb in [{3}]
+; sprite_lsb in [{4}]
+        mac     draw_char
+            ldx     [{1}]
+            ldy     #$00            ; draw 1st char
+            draw_sprite [{2}], [{3}], [{4}]
+            inx                     ; draw 2nd char
+            ldy     #$16
+            draw_sprite [{2}], [{3}], [{4}]
+            inx                     ; draw 3rd char
+            ldy     #$01
+            draw_sprite [{2}], [{3}], [{4}]
+            inx                     ; draw 4th char
+            ldy     #$17
+            draw_sprite [{2}], [{3}], [{4}]
+        endm
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; needs sprite to erase in [{1}],
+; sprite_clr in [{2}],
+; sprite_clr_lsb in [{3}]
+; sprite_lsb in [{4}]
+        mac     erase_char
+            ldx     [{1}]
+            ldy     #$00            ; draw 1st char
+            draw_sprite [{2}], [{3}], [{4}]
+            ldy     #$16
+            draw_sprite [{2}], [{3}], [{4}]
+            ldy     #$01
+            draw_sprite [{2}], [{3}], [{4}]
+            ldy     #$17
+            draw_sprite [{2}], [{3}], [{4}]
+        endm
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; needs offset in [{1}]
+; needs sprite lsb in [{2}]
+; needs sprite msb in [{3}]
+; needs sprite clr lsb in [{4}]
+; needs sprite clr msb in [{5}]
+; needs add/subOffset subroutine in [{6}]
+        mac     update_tree_addr
+            lda     [{1}]
+            sta     OFFSET
+            lda     [{2}]
+            ldx     [{3}]
+            jsr     [{6}]
+            sta     TREE_MSB
+            lda     LSB
+            sta     TREE_LSB
+
+            lda     [{4}]
+            ldx     [{5}]
+            jsr     [{6}]
+            sta     TREE_CLR_MSB
+            lda     LSB
+            sta     TREE_CLR_LSB
+
+            ldy     #$00
+            lda     (TREE_LSB),y
+            cmp     #PATH
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- Animation_Test/sprites.asm
+++ Animation_Test/sprites.asm
@@ -1,40 +1,40 @@
-d;;; ----- DRAWING ROUTINES
+d       ;;; ----- DRAWING ROUTINES
 
 place_letter:
-	lda	LFSR
-	jsr	random
+        lda     LFSR
+        jsr     random
 
-	sta	LETTER_LSB
-	sta	LETTER_CLR_LSB	
+        sta     LETTER_LSB
+        sta     LETTER_CLR_LSB
 
-	ldy #$0
-	lda (LETTER_LSB),y
-	cmp #PATH
-	bne place_letter
+        ldy     #$0
+        lda     (LETTER_LSB),y
+        cmp     #PATH
+        bne     place_letter
 
-	ldy #$1
-	lda (LETTER_LSB),y
-	cmp #PATH
-	bne place_letter
+        ldy     #$1
+        lda     (LETTER_LSB),y
+        cmp     #PATH
+        bne     place_letter
 
-	ldy	#$0
-	lda	#ITEM_LETTER
-	sta	(LETTER_LSB),y
-	lda	#$01
-	sta	(LETTER_CLR_LSB),y
+        ldy     #$0
+        lda     #ITEM_LETTER
+        sta     (LETTER_LSB),y
+        lda     #$01
+        sta     (LETTER_CLR_LSB),y
 
-	iny
-	lda	#ITEM_LETTER + 1
-	sta	(LETTER_LSB),y
-	lda	#$01
-	sta	(LETTER_CLR_LSB),y
-	
-	rts
+        iny
+        lda     #ITEM_LETTER + 1
+        sta     (LETTER_LSB),y
+        lda     #$01
+        sta     (LETTER_CLR_LSB),y
+
+        rts
 
 erase:
-	erase_char #PATH, CURR_CLR, SPRITE_CLR_LSB, SPRITE_LSB
-	;jsr erase_env
-	rts
+        erase_char #PATH, CURR_CLR, SPRITE_CLR_LSB, SPRITE_LSB
+        ;jsr erase_env
+        rts
 
 ; erase_env: subroutine
 ; .draw_NW_tree
@@ -110,60 +110,60 @@ erase:
 
 ;;; ---- SPRITES AND STUFF
 sprite:
-	;;  sprite facing forward
-	BYTE	21,85,118,89,89,30,90,85
-	BYTE	7,31,31,31,127,85,4,4
-	BYTE	84,84,164,152,152,172,164,85
-	BYTE	208,100,100,220,253,85,16,16
+        ;;  sprite facing forward
+        BYTE    21,85,118,89,89,30,90,85
+        BYTE    7,31,31,31,127,85,4,4
+        BYTE    84,84,164,152,152,172,164,85
+        BYTE    208,100,100,220,253,85,16,16
 
-	;; sprite facing backward
-	BYTE	21,85,117,85,85,85,85,21
-	BYTE	7,31,31,127,127,85,4,4
-	BYTE	84,85,85,85,85,85,85,84
-	BYTE	208,244,244,253,253,85,16,16
-	
-	;; sprite facing to the right
-	BYTE	21,85,117,94,86,87,22,85
-	BYTE	7,31,31,31,127,85,16,16
-	BYTE	84,84,164,100,105,164,164,80
-	BYTE	208,244,217,217,244,84,16,16
-	
-	;; sprite facing to the left
-	BYTE	21,21,26,25,105,26,26,5
-	BYTE	7,31,103,103,31,21,4,4
-	BYTE	84,85,85,149,149,213,148,85
-	BYTE	208,244,244,244,253,85,4,4
+        ;; sprite facing backward
+        BYTE    21,85,117,85,85,85,85,21
+        BYTE    7,31,31,127,127,85,4,4
+        BYTE    84,85,85,85,85,85,85,84
+        BYTE    208,244,244,253,253,85,16,16
 
-	;; sprite facing forward -- blinking
-	BYTE	0,21,85,118,90,89,30,90
-	BYTE	85,7,31,31,31,127,85,4
-	BYTE	0,84,84,164,168,152,172,164
-	BYTE	85,208,100,100,220,253,85,16
-	
-	;; sprite facing backward -- blinking
-	BYTE	0,21,85,117,85,85,85,85
-	BYTE	21,7,31,31,127,127,85,4
-	BYTE	0,84,85,85,85,85,85,85
-	BYTE	84,208,244,244,253,253,85,16
+        ;; sprite facing to the right
+        BYTE    21,85,117,94,86,87,22,85
+        BYTE    7,31,31,31,127,85,16,16
+        BYTE    84,84,164,100,105,164,164,80
+        BYTE    208,244,217,217,244,84,16,16
 
-	;; sprite facing to the left -- blinking
-	BYTE	0,21,85,117,94,86,87,22
-	BYTE	21,7,31,31,31,127,85,16
-	BYTE	0,84,84,164,164,105,164,164
-	BYTE	80,208,244,217,217,244,84,16
+        ;; sprite facing to the left
+        BYTE    21,21,26,25,105,26,26,5
+        BYTE    7,31,103,103,31,21,4,4
+        BYTE    84,85,85,149,149,213,148,85
+        BYTE    208,244,244,244,253,85,4,4
 
-	;; sprite facing to the right -- blinking
-	BYTE	0,21,21,26,26,105,26,26
-	BYTE	5,7,31,103,103,31,21,4
-	BYTE	0,84,85,85,149,149,213,148
-	BYTE	85,208,244,244,244,253,85,4
+        ;; sprite facing forward -- blinking
+        BYTE    0,21,85,118,90,89,30,90
+        BYTE    85,7,31,31,31,127,85,4
+        BYTE    0,84,84,164,168,152,172,164
+        BYTE    85,208,100,100,220,253,85,16
+
+        ;; sprite facing backward -- blinking
+        BYTE    0,21,85,117,85,85,85,85
+        BYTE    21,7,31,31,127,127,85,4
+        BYTE    0,84,85,85,85,85,85,85
+        BYTE    84,208,244,244,253,253,85,16
+
+        ;; sprite facing to the left -- blinking
+        BYTE    0,21,85,117,94,86,87,22
+        BYTE    21,7,31,31,31,127,85,16
+        BYTE    0,84,84,164,164,105,164,164
+        BYTE    80,208,244,217,217,244,84,16
+
+        ;; sprite facing to the right -- blinking
+        BYTE    0,21,21,26,26,105,26,26
+        BYTE    5,7,31,103,103,31,21,4
+        BYTE    0,84,85,85,149,149,213,148
+        BYTE    85,208,244,244,244,253,85,4
 
 tree1:
-	BYTE	1,1,7,1,7,30,7,30
-	BYTE	26,122,30,122,85,1,1,5
-	BYTE	64,64,144,64,144,164,144,164
-	BYTE	164,169,164,169,85,64,64,80
+        BYTE    1,1,7,1,7,30,7,30
+        BYTE    26,122,30,122,85,1,1,5
+        BYTE    64,64,144,64,144,164,144,164
+        BYTE    164,169,164,169,85,64,64,80
 
-letter:	
-	BYTE	0,15,11,13,14,15,15,0
-	BYTE	0,240,208,176,112,240,240,0
+letter:
+        BYTE    0,15,11,13,14,15,15,0
+        BYTE    0,240,208,176,112,240,240,0
